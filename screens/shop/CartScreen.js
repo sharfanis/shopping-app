38db64dbc6acf47edbae9cb2d97956082167280b
Shabih <Shabih.sharfani@gmail.com>
2021-02-03 18:15:50 -0600

    chore(): laying out the cart items and also disabling button when there are no items in cart

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -2,20 +2,54 @@ import React from "react";
 import { View, Text, FlatList, Button, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
 import color from "../../constants/color";
+import ProductItem from "../../components/shop/ProductItem";
 
 const CartScreen = (props) => {
   const cartTotalAmount = useSelector((state) => state.cart.totalAmount);
+  const cartItems = useSelector((state) => {
+    const transformedCartItems = [];
+    for (const key in state.cart.items) {
+      transformedCartItems.push({
+        productId: key,
+        productTitle: state.cart.items[key].productTitle,
+        productPrice: state.cart.items[key].productPrice,
+        quantity: state.cart.items[key].quantity,
+        sum: state.cart.items[key].sum,
+      });
+    }
+    return transformedCartItems;
+  });
+
+  const renderProducts = (product) => {
+    return (
+      <View style={styles.summary}>
+        <Text>{product.item.productTitle}</Text>
+        <Text>{product.item.quantity}</Text>
+        <Text>{product.item.sum}</Text>
+      </View>
+    );
+  };
 
   return (
     <View style={styles.screen}>
       <View style={styles.summary}>
         <Text style={styles.summaryText}>
-          Total : <Text style={styles.amount}>${cartTotalAmount.toFixed(2)}</Text>
+          Total :
+          <Text style={styles.amount}> ${cartTotalAmount.toFixed(2)}</Text>
         </Text>
-        <Button title="Order Now" />
+        <Button
+          color={color.accent}
+          title="Order Now"
+          disabled={cartItems.length === 0}
+        />
       </View>
       <View>
         <Text>CART ITEMS</Text>
+        <FlatList
+          data={cartItems}
+          keyExtractor={(item) => item.id}
+          renderItem={renderProducts}
+        />
       </View>
     </View>
   );
@@ -38,13 +72,22 @@ const styles = StyleSheet.create({
     shadowRadius: 8,
     elevation: 5,
     borderRadius: 10,
-    backgroundColor: "white"
+    backgroundColor: "white",
+  },
+  cartList: {
+    shadowColor: color.black,
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: "white",
   },
   summaryText: {
-    fontSize: 18
+    fontSize: 18,
   },
   amount: {
-    color: color.accent
+    color: color.primary,
   },
 });
 
