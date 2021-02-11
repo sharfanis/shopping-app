3afdd852cb99482e3953ffbdbc3376489086a089
Shabih <Shabih.sharfani@gmail.com>
2021-02-11 14:27:53 -0600

    chore(): Used moment to render date same as IOS
    Signed-off-by: Shabih <Shabih.sharfani@gmail.com>

--- models/order.js
+++ models/order.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 class Order {
   constructor(id, items, totalAmount, date) {
     this.id = id; // id of order not the items in the order. Standalone ID.
@@ -7,13 +8,16 @@ class Order {
   }
 
   get readableDate() {
-    return this.date.toLocaleDateString("en-EN", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-    });
+
+    // return this.date.toLocaleDateString("en-EN", {
+    //   year: "numeric",
+    //   month: "long",
+    //   day: "numeric",
+    //   hour: "2-digit",
+    //   minute: "2-digit",
+    // });
+    // this will help the Android to render the same as IOS 
+    return moment(this.date).format('MMMM DD YYYY, hh:mm');
   }
 }
 
